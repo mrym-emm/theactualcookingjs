2529babde2ae4587ce82429ea1ab65776105b154
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-03-01 03:27:05 +0800

    do while loopssss.

---
+++ 12 flow_dowhileloops/app.js
@@ -0,0 +1,21 @@
+// do while loops
+
+let i = 1;
+
+do {
+  console.log(`Hello World ${i}`);
+  i++;
+} while (i <= 5); // while (condition)
+
+console.log("Exercise");
+// Print "Your Name" 400 times to the console using do-while-loop.
+// Iteration should start from 20.
+
+let j = 20;
+do {
+  console.log(`${j} mm`);
+  j++;
+} while (j <= 200);
+
+// the do while loop is read as please do (execute this block of code);
+// while this condition is true. very similar to while loops

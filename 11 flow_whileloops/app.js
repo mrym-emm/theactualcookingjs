f067751f96db2ddd8a3198e03934acff3529d2ab
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-03-01 03:21:06 +0800

    while loops. ALWAYS INCREMENT

---
+++ 11 flow_whileloops/app.js
@@ -0,0 +1,24 @@
+// while loops --> run until its false
+// syntax:
+// while (condition) {
+// code block to be executed
+//    }
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(`${i} hello world`);
+  i++; // without this it will be stuck in an infinite looooop
+}
+
+console.log("Exercise");
+
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
+
+let j = 10;
+
+while (j <= 100) {
+  console.log(`${j} mm`);
+  j++;
+}

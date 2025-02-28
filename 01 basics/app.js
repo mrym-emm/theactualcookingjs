fc1575b9b4f37cc9a323babf112732df520ae3e4
mrym-emm <153648180+mrym-emm@users.noreply.github.com>
2025-02-28 17:21:34 +0800

    exploring console.log() mostly.  a hint of alert

---
+++ 01 basics/app.js
@@ -0,0 +1,18 @@
+// shows pop up
+alert("Collosal losssss");
+
+// console is an object
+
+// anything tht i write in the parantheses will logged in the browsers console
+console.log("Howdy doody");
+console.log(2 + 2);
+
+console.clear(); // to clear console
+
+console.log({ name: "mrym" }); // this is an object
+
+console.warn("This is a warning"); // is a warning
+
+console.error("This is an error"); // is an error
+
+console.table({ name: "mrym", age: 26 });
